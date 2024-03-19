fdd4a4524f6570ca31751cc3f4d2ba075262aa94
跨性别 <cvyl@pm.me>
2024-03-19 13:58:26 +0100

    feat: new code

--- app/routes/_index.tsx
+++ app/routes/_index.tsx
@@ -56,41 +56,41 @@ export default function Index() {
 
 
         <section className="mb-8 px-10 mx-auto">
-  <div className="bg-white shadow-md rounded-lg p-6">
-    <h1>Wat is de overheid nu aan het doen?</h1>
-    <h2 className="text-xl font-semibold mb-2">Klimaatadaptatie:</h2>
-    <div className="mt-1 grid grid-cols-1 gap-6 md:grid-cols-4 justify-center">
-      {/* First repetition */}
-      <div className="bg-[#f4f4f4] border border-gray-detailBorder rounded-lg overflow-hidden relative">
-        <img className="h-auto w-full rounded-lg" src="https://external-content.duckduckgo.com/iu/?u=https%3A%2F%2Ftse1.mm.bing.net%2Fth%3Fid%3DOIP.JEBPIeV7s3lZ9QJ7DyfS4AHaEK%26pid%3DApi&f=1&ipt=de31728e4a63e4f6663124ae18fdde919cb268006de9cca4ce9b7193c8ba9ddf&ipo=images" alt="description" />
-        <figcaption className="absolute bottom-0 left-0 right-0 bg-green-600 text-white p-4 text-center">
-          Versterk dijken en verbreed rivieren om overstromingen te voorkomen.
-        </figcaption>
-      </div>
-      {/* Second repetition */}
-      <div className="bg-[#f4f4f4] border border-gray-detailBorder rounded-lg overflow-hidden relative">
-        <img className="h-auto w-full rounded-lg" src="https://external-content.duckduckgo.com/iu/?u=https%3A%2F%2Faannemer-gigant-nl.imgix.net%2Fwp-content%2Fuploads%2F2019%2F09%2Fzonnepanelen-plaatsen-1.jpg%3Fauto%3Dformat%252Ccompress%26w%3D1024%26h%3D1024%26dpr%3D3&f=1&nofb=1&ipt=3c33ac7a5d56b461994d9d141e892651679dbc37de77f7ac22881ed77c03d0b2&ipo=images" alt="description" />
-        <figcaption className="absolute bottom-0 left-0 right-0 bg-green-600 text-white p-4 text-center">
-          We zijn met subsidies aan het zorgen dat mensen meer zonnepanelen aanleggen.
-        </figcaption>
-      </div>
-      {/* Third repetition */}
-      <div className="bg-[#f4f4f4] border border-gray-detailBorder rounded-lg overflow-hidden relative">
-        <img className="h-auto w-full rounded-lg" src="https://fotobankna.nl/wp-content/uploads/vg096denhaag2014-480x360-81.jpg" alt="description" />
-        <figcaption className="absolute bottom-0 left-0 right-0 bg-green-600 text-white p-4 text-center">
-          Wegen onder parken maken.
-        </figcaption>
-      </div>
-      {/* Fourth repetition */}
-      <div className="bg-[#f4f4f4] border border-gray-detailBorder rounded-lg overflow-hidden relative">
-        <img className="h-auto w-full rounded-lg" src="https://www.teng-groep.nl/wp-content/uploads/20210521_TenG_Groendak-Doorn-19-1024x683.jpg" alt="description" />
-        <figcaption className="absolute bottom-0 left-0 right-0 bg-green-600 text-white p-4 text-center">
-          Implementeer groene infrastructuur, zoals groendaken, groene gevels en stadsbossen, om de stad te vergroenen en de leefbaarheid te verbeteren.
-        </figcaption>
-      </div>
-    </div>
-  </div>
-</section>
+          <div className="bg-white shadow-md rounded-lg p-6">
+            <h1>Wat is de overheid nu aan het doen?</h1>
+            <h2 className="text-xl font-semibold mb-2">Klimaatadaptatie:</h2>
+            <div className="mt-1 grid grid-cols-1 gap-6 md:grid-cols-4 justify-center">
+              {/* First repetition */}
+              <div className="bg-[#f4f4f4] border border-gray-detailBorder rounded-lg overflow-hidden relative">
+                <img className="h-auto w-full rounded-lg" src="https://external-content.duckduckgo.com/iu/?u=https%3A%2F%2Ftse1.mm.bing.net%2Fth%3Fid%3DOIP.JEBPIeV7s3lZ9QJ7DyfS4AHaEK%26pid%3DApi&f=1&ipt=de31728e4a63e4f6663124ae18fdde919cb268006de9cca4ce9b7193c8ba9ddf&ipo=images" alt="description" />
+                <figcaption className="absolute bottom-0 left-0 right-0 bg-green-600 text-white p-4 text-center">
+                  Versterk dijken en verbreed rivieren om overstromingen te voorkomen.
+                </figcaption>
+              </div>
+              {/* Second repetition */}
+              <div className="bg-[#f4f4f4] border border-gray-detailBorder rounded-lg overflow-hidden relative">
+                <img className="h-auto w-full rounded-lg" src="https://external-content.duckduckgo.com/iu/?u=https%3A%2F%2Faannemer-gigant-nl.imgix.net%2Fwp-content%2Fuploads%2F2019%2F09%2Fzonnepanelen-plaatsen-1.jpg%3Fauto%3Dformat%252Ccompress%26w%3D1024%26h%3D1024%26dpr%3D3&f=1&nofb=1&ipt=3c33ac7a5d56b461994d9d141e892651679dbc37de77f7ac22881ed77c03d0b2&ipo=images" alt="description" />
+                <figcaption className="absolute bottom-0 left-0 right-0 bg-green-600 text-white p-4 text-center">
+                  We zijn met subsidies aan het zorgen dat mensen meer zonnepanelen aanleggen.
+                </figcaption>
+              </div>
+              {/* Third repetition */}
+              <div className="bg-[#f4f4f4] border border-gray-detailBorder rounded-lg overflow-hidden relative">
+                <img className="h-auto w-full rounded-lg" src="https://fotobankna.nl/wp-content/uploads/vg096denhaag2014-480x360-81.jpg" alt="description" />
+                <figcaption className="absolute bottom-0 left-0 right-0 bg-green-600 text-white p-4 text-center">
+                  Wegen onder parken maken.
+                </figcaption>
+              </div>
+              {/* Fourth repetition */}
+              <div className="bg-[#f4f4f4] border border-gray-detailBorder rounded-lg overflow-hidden relative">
+                <img className="h-auto w-full rounded-lg" src="https://www.teng-groep.nl/wp-content/uploads/20210521_TenG_Groendak-Doorn-19-1024x683.jpg" alt="description" />
+                <figcaption className="absolute bottom-0 left-0 right-0 bg-green-600 text-white p-4 text-center">
+                  Implementeer groene infrastructuur, zoals groendaken, groene gevels en stadsbossen, om de stad te vergroenen en de leefbaarheid te verbeteren.
+                </figcaption>
+              </div>
+            </div>
+          </div>
+        </section>
 
 
 
@@ -98,32 +98,37 @@ export default function Index() {
 
         <section className="mb-8 px-10 mx-auto">
           <div className="bg-white shadow-md rounded-lg p-6">
+            <h5 className="mb-2 text-2xl font-bold tracking-tight text-gray-900 dark:text-white">Wat kan nog meer gedaan worden?</h5>
+            <div className="grid grid-cols-1 gap-6 md:grid-cols-2 lg:grid-cols-4">
+              {/* First bullet point */}
+              <div className="block max-w-sm p-6 bg-white border border-gray-200 rounded-lg shadow hover:bg-gray-100 dark:bg-gray-800 dark:border-gray-700 dark:hover:bg-gray-700">
+                <h5 className="mb-2 text-2xl font-bold tracking-tight text-gray-900 dark:text-white"> </h5>
+                <p className="font-normal text-gray-700 dark:text-gray-400">Leg meer groene ruimtes aan, zoals parken en plantsoenen.</p>
+              </div>
+              {/* Second bullet point */}
+              <div className="block max-w-sm p-6 bg-white border border-gray-200 rounded-lg shadow hover:bg-gray-100 dark:bg-gray-800 dark:border-gray-700 dark:hover:bg-gray-700">
+                <h5 className="mb-2 text-2xl font-bold tracking-tight text-gray-900 dark:text-white"> </h5>
+                <p className="font-normal text-gray-700 dark:text-gray-400">Integreer groen in stedelijke planning en bouwvoorschriften.</p>
+              </div>
+              {/* Third bullet point */}
+              <div className="block max-w-sm p-6 bg-white border border-gray-200 rounded-lg shadow hover:bg-gray-100 dark:bg-gray-800 dark:border-gray-700 dark:hover:bg-gray-700">
+                <h5 className="mb-2 text-2xl font-bold tracking-tight text-gray-900 dark:text-white"> </h5>
+                <p className="font-normal text-gray-700 dark:text-gray-400">Betrek burgers bij het vergroenen van hun eigen omgeving.</p>
+              </div>
+              {/* Fourth bullet point */}
+              <div className="block max-w-sm p-6 bg-white border border-gray-200 rounded-lg shadow hover:bg-gray-100 dark:bg-gray-800 dark:border-gray-700 dark:hover:bg-gray-700">
+                <h5 className="mb-2 text-2xl font-bold tracking-tight text-gray-900 dark:text-white"> </h5>
+                <p className="font-normal text-gray-700 dark:text-gray-400">Informeer burgers en bedrijven over klimaatverandering en hoe ze kunnen bijdragen aan een groenere toekomst.</p>
+              </div>
+              {/* Fifth bullet point */}
+              <div className="block max-w-sm p-6 bg-white border border-gray-200 rounded-lg shadow hover:bg-gray-100 dark:bg-gray-800 dark:border-gray-700 dark:hover:bg-gray-700">
+              <h5 className="mb-2 text-2xl font-bold tracking-tight text-gray-900 dark:text-white"> </h5>
+                <p className="font-normal text-gray-700 dark:text-gray-400">Organiseer workshops, lezingen en campagnes om bewustwording te vergroten.</p>
+              </div>
+            </div>
+          </div >
+        </section >
 
-
-            <h1 className="text-xl font-semibold mb-2">Wat kan nog meer gedaan worden?</h1>
-
-
-
-            <h2 className="text-l font-semibold mb-2">Vergroening van steden:</h2>
-            <ul className="list-disc pl-6 mb-4">
-              <li>Leg meer groene ruimtes aan, zoals parken en plantsoenen.</li>
-              <li>Integreer groen in stedelijke planning en bouwvoorschriften.</li>
-              <li>Betrek burgers bij het vergroenen van hun eigen omgeving.</li>
-            </ul>
-            <h2 className="text-xl font-semibold mb-2">Klimaatmitigatie:</h2>
-            <ul className="list-disc pl-6 mb-4">
-              <li>Verminder de uitstoot van broeikasgassen door over te stappen op duurzame energiebronnen en energie-efficiëntie te bevorderen.</li>
-              <li>Onderzoek innovatieve oplossingen, zoals groene waterstof, om de energietransitie te versnellen.</li>
-            </ul>
-            <h2 className="text-xl font-semibold mb-2">Bewustwording en educatie:</h2>
-            <ul className="list-disc pl-6 mb-4">
-              <li>Informeer burgers en bedrijven over klimaatverandering en hoe ze kunnen bijdragen aan een groenere toekomst.</li>
-              <li>Organiseer workshops, lezingen en campagnes om bewustwording te vergroten.</li>
-            </ul>
-
-
-          </div>
-        </section>
 
 
         <section className="mb-8">
@@ -182,7 +187,7 @@ export default function Index() {
             </div>
           </div>
         </section>
-      </main>
+      </main >
       <footer className="bg-green-800 py-4 mt-8">
         <div className="container mx-auto text-center text-white">
           {/* Add footer content here */}
